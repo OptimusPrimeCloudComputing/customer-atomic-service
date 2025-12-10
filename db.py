6d88c11d5a4f11231dc85205c674f7d5e6e3ca37
rs4777 <rs4777@columbia.edu>
2025-12-10 09:56:38 -0500

    Changed DB conn settings

--- db.py
+++ db.py
@@ -1,22 +1,52 @@
+# import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+#
+# DB_USER = os.getenv("DB_USER", "myapp_user")
+# DB_PASS = os.getenv("DB_PASS", "Password_123")
+# DB_NAME = os.getenv("DB_NAME", "customer")
+#
+# # Detect environment
+# CLOUD_SQL_CONNECTION_NAME = os.getenv("CLOUD_SQL_CONNECTION_NAME")
+#
+# if CLOUD_SQL_CONNECTION_NAME:
+#     DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@/{DB_NAME}?unix_socket=/cloudsql/{CLOUD_SQL_CONNECTION_NAME}"
+#     print("Using Cloud SQL Unix socket connection")
+# else:
+#     DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+#     DB_PORT = os.getenv("DB_PORT", "3306")
+#     DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+#     print(f"Using local connection via {DB_HOST}:{DB_PORT}")
+#
+# engine = create_engine(
+#     DATABASE_URL,
+#     pool_pre_ping=True,
+# )
+#
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
+#
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DB_USER = os.getenv("DB_USER", "myapp_user")
 DB_PASS = os.getenv("DB_PASS", "Password_123")
-DB_NAME = os.getenv("DB_NAME", "customer")
+DB_NAME = os.getenv("DB_NAME", "addresses")
+DB_HOST = os.getenv("DB_HOST", "34.171.164.80")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+print(f"Using local connection via {DB_HOST}:{DB_PORT}")
 
-# Detect environment
+
 CLOUD_SQL_CONNECTION_NAME = os.getenv("CLOUD_SQL_CONNECTION_NAME")
-
-if CLOUD_SQL_CONNECTION_NAME:
-    DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@/{DB_NAME}?unix_socket=/cloudsql/{CLOUD_SQL_CONNECTION_NAME}"
-    print("Using Cloud SQL Unix socket connection")
-else:
-    DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
-    DB_PORT = os.getenv("DB_PORT", "3306")
-    DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-    print(f"Using local connection via {DB_HOST}:{DB_PORT}")
 
 engine = create_engine(
     DATABASE_URL,
